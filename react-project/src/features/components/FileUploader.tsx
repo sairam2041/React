734d17a2012e090067d71bc463dd740e8b7ed01b
sairam2041 <63296834+sairam2041@users.noreply.github.com>
2025-09-17 22:17:15 +0900

    ファイルのプレビュー機能の追加

--- react-project/src/features/components/FileUploader.tsx
+++ react-project/src/features/components/FileUploader.tsx
@@ -1,19 +1,47 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import './FileUploader.css';
 
 export const FileUploader = (): React.JSX.Element => {
   const [file, setFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0] || null;
     setFile(selectedFile);
+
+    if(selectedFile) {
+      const objectUrl = URL.createObjectURL(selectedFile);
+      setPreviewUrl(objectUrl);
+    } else {
+      setPreviewUrl(null);
+    }
   };
+
+  const handleResetPreview = () => {
+    if(previewUrl) {
+      URL.revokeObjectURL(previewUrl);
+      setPreviewUrl(null);
+      setFile(null);
+
+      // ファイル名の表示を消す
+      if (fileInputRef.current) {
+        fileInputRef.current.value = '';
+      }
+    }
+  }
 
   return (
     <div className="FileUploader">
       <div className="App-form">
-        <input name="file" type="file" accept="image/*" onChange={handleChange} />
+        <input name="file" type="file" accept="image/*" onChange={handleChange} ref={fileInputRef} />
         <input type="button" disabled={!file} value="送信" />
+        {previewUrl && (
+          <div className="ImagePreview">
+            <img src={previewUrl} alt="プレビュー" />
+            <button className="ResetButton" onClick={handleResetPreview}>初期化</button>
+          </div>
+         )}
       </div>
     </div>
   );
